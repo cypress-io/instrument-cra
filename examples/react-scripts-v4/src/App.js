f19c8d9c4df3487a863929d6a0b0a2ca41430858
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-26 15:59:13 -0400

    feat: support react-scripts v4 (#198)

---
+++ examples/react-scripts-v4/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+import { getRandomNumber } from './calc'
+import Child from './Child'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="random">This is a random number {getRandomNumber()}</p>
+        <Child />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
